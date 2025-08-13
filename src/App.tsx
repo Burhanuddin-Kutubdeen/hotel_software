b234fd6f2624ef4e58ff48193481d969f64552a2
burhan <burhan.kutub@gmail.com>
2025-08-13 17:16:22 +0530

    Chore: Small change to trigger new commit
    - Added a comment to src/App.tsx to create a new commit for testing purposes.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AppProvider } from '@/contexts/AppContext';
 import AuthWrapper from '@/components/AuthWrapper';
 import './App.css';
 
+// This is a small change to create a new commit.
 const App: React.FC = () => {
   return (
     <AppProvider>
